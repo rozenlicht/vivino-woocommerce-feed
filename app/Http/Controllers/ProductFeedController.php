c4ad8b3a68fef02959c59dfbb077c17e280b7aeb
Bart Verhaegh <bart@closure.nl>
2021-02-26 08:42:04 +0100

    Fix: vivino feed name change

--- app/Http/Controllers/ProductFeedController.php
+++ app/Http/Controllers/ProductFeedController.php
@@ -21,6 +21,7 @@ class ProductFeedController extends Controller
             $product->wine_name = null;
             $product->vintage = null;
             $product->alcohol = null;
+            $product->appellation = null;
             preg_match('/\b(19|20)\d{2}\b/', $product->name, $matches);
             if((count($matches) > 0 || Str::contains($product->name, ' NV'))
                 && $product->attributes->where('name', "Producent")->count() > 0
@@ -33,6 +34,7 @@ class ProductFeedController extends Controller
                 $product->vintage = count($matches) > 0 ? $matches[0] : 'NV';
                 $product->producer = $product->attributes->where('name', "Producent")->first()->options[0];
                 $product->wine_name = ucwords($product->attributes->where('name', "Vivino naam")->first()->options[0]);
+                $product->appellation = ucwords($product->attributes->where('name', "Streek")->first()->options[0]);
             }
             return $product;
         });
